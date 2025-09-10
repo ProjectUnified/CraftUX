fef428caf36e42951ff7b1af88bee343c508bd4c
hsgamer <huynhqtienvtag@gmail.com>
2025-09-10 09:17:41 +0700

    docs on Button and Mask

--- common/src/main/java/io/github/projectunified/craftux/common/Button.java
+++ common/src/main/java/io/github/projectunified/craftux/common/Button.java
@@ -5,9 +5,25 @@ import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+/**
+ * The button
+ */
 public interface Button {
+    /**
+     * Apply actions to the action item
+     *
+     * @param uuid       the uuid of the player
+     * @param actionItem the action item
+     * @return true if any action was applied. Can return false in a conditional case (e.g. predicate button)
+     */
     boolean apply(@NotNull UUID uuid, @NotNull ActionItem actionItem);
 
+    /**
+     * Get a consumer that applies actions to the action item
+     *
+     * @param uuid the uuid of the player
+     * @return the consumer
+     */
     default Consumer<ActionItem> apply(@NotNull UUID uuid) {
         return actionItem -> apply(uuid, actionItem);
     }
--- common/src/main/java/io/github/projectunified/craftux/common/Mask.java
+++ common/src/main/java/io/github/projectunified/craftux/common/Mask.java
@@ -7,6 +7,15 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+/**
+ * The mask that maps positions to action item consumers
+ */
 public interface Mask {
+    /**
+     * Get a map of positions to action item consumers
+     *
+     * @param uuid the uuid of the player
+     * @return the map, or null if no items should be displayed. Can return null in a conditional case (e.g. predicate mask)
+     */
     @Nullable Map<Position, Consumer<ActionItem>> apply(@NotNull UUID uuid);
 }
