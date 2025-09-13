18bc0e386eaad8da100d83719c9e411d1825a15b
hsgamer <huynhqtienvtag@gmail.com>
2025-09-13 14:16:25 +0700

    getActionMap in Mask

--- common/src/main/java/io/github/projectunified/craftux/common/Mask.java
+++ common/src/main/java/io/github/projectunified/craftux/common/Mask.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * The mask that maps positions to action item consumers
@@ -18,4 +19,22 @@ public interface Mask {
      * @return the map, or null if no items should be displayed. Can return null in a conditional case (e.g. predicate mask)
      */
     @Nullable Map<Position, Consumer<ActionItem>> apply(@NotNull UUID uuid);
+
+    /**
+     * Get a map of positions to action items
+     *
+     * @param uuid the uuid of the player
+     * @return the map, or null if no items should be displayed. Can return null in a conditional case (e.g. predicate mask)
+     */
+    default @Nullable Map<Position, ActionItem> getActionMap(UUID uuid) {
+        Map<Position, Consumer<ActionItem>> map = apply(uuid);
+        if (map == null) return null;
+        return map.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+                    ActionItem actionItem = new ActionItem();
+                    entry.getValue().accept(actionItem);
+                    return actionItem;
+                }));
+    }
 }
