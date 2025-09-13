c49c2a0193a9c3e52e7c8f79cc460a4246d16704
hsgamer <huynhqtienvtag@gmail.com>
2025-09-13 15:57:45 +0700

    add spigot-button for spigot-specific button

---
+++ spigot-button/src/main/java/io/github/projectunified/craftux/spigot/button/InputButton.java
@@ -0,0 +1,78 @@
+package io.github.projectunified.craftux.spigot.button;
+
+import io.github.projectunified.craftux.common.ActionItem;
+import io.github.projectunified.craftux.common.Button;
+import io.github.projectunified.craftux.common.Element;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.BiFunction;
+
+/**
+ * The button that stores the input item
+ */
+public class InputButton implements Element, Button {
+    private final Map<UUID, ItemStack> map = new IdentityHashMap<>();
+    private BiFunction<@NotNull UUID, @Nullable ItemStack, @Nullable ItemStack> displayItemFunction = (uuid, item) -> item;
+
+    @Override
+    public boolean apply(@NotNull UUID uuid, @NotNull ActionItem actionItem) {
+        actionItem.setItem(displayItemFunction.apply(uuid, getInputItem(uuid)));
+        actionItem.setAction(InventoryClickEvent.class, event -> {
+            ItemStack cursorItem = Optional.ofNullable(event.getCursor())
+                    .filter(itemStack -> itemStack.getType() != Material.AIR)
+                    .map(ItemStack::clone)
+                    .orElse(null);
+            ItemStack storeItem = getInputItem(uuid);
+            event.getWhoClicked().setItemOnCursor(storeItem);
+            setInputItem(uuid, cursorItem);
+        });
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        map.clear();
+    }
+
+    /**
+     * Set the input item for the unique id
+     *
+     * @param uuid      the unique id
+     * @param itemStack the item, or null to remove the input item
+     */
+    public void setInputItem(@NotNull UUID uuid, @Nullable ItemStack itemStack) {
+        map.compute(uuid, (uuid1, item) -> itemStack);
+    }
+
+    /**
+     * Get the input item for the unique id
+     *
+     * @param uuid the unique id
+     * @return the item
+     */
+    @Nullable
+    public ItemStack getInputItem(@NotNull UUID uuid) {
+        return map.get(uuid);
+    }
+
+    /**
+     * Set the function to display the item on the GUI
+     *
+     * @param displayItemFunction the function
+     * @return this instance
+     */
+    @Contract("_ -> this")
+    public InputButton setDisplayItemFunction(@NotNull BiFunction<UUID, ItemStack, ItemStack> displayItemFunction) {
+        this.displayItemFunction = displayItemFunction;
+        return this;
+    }
+}
---
+++ spigot-button/src/main/java/io/github/projectunified/craftux/spigot/button/OutputButton.java
@@ -0,0 +1,77 @@
+package io.github.projectunified.craftux.spigot.button;
+
+import io.github.projectunified.craftux.common.ActionItem;
+import io.github.projectunified.craftux.common.Button;
+import io.github.projectunified.craftux.common.Element;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.BiFunction;
+
+/**
+ * The button that stores the output button for the viewer
+ */
+public class OutputButton implements Element, Button {
+    private final Map<UUID, ItemStack> map = new IdentityHashMap<>();
+    private BiFunction<@NotNull UUID, @Nullable ItemStack, @Nullable ItemStack> displayItemFunction = (uuid, item) -> item;
+
+    @Override
+    public boolean apply(@NotNull UUID uuid, @NotNull ActionItem actionItem) {
+        actionItem.setItem(displayItemFunction.apply(uuid, getOutputItem(uuid)));
+        actionItem.setAction(InventoryClickEvent.class, event -> {
+            ItemStack item = event.getCursor();
+            if (item != null && item.getType() != Material.AIR) {
+                return;
+            }
+            ItemStack storeItem = getOutputItem(uuid);
+            event.getWhoClicked().setItemOnCursor(storeItem);
+            setOutputItem(uuid, null);
+        });
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        map.clear();
+    }
+
+    /**
+     * Set the output item for the unique id
+     *
+     * @param uuid      the unique id
+     * @param itemStack the item, or null to remove the output button
+     */
+    public void setOutputItem(@NotNull UUID uuid, @Nullable ItemStack itemStack) {
+        map.compute(uuid, (uuid1, item) -> itemStack);
+    }
+
+    /**
+     * Get the output item for the unique id
+     *
+     * @param uuid the unique id
+     * @return the item
+     */
+    @Nullable
+    public ItemStack getOutputItem(@NotNull UUID uuid) {
+        return map.get(uuid);
+    }
+
+    /**
+     * Set the function to display the item on the GUI
+     *
+     * @param displayItemFunction the function
+     * @return this instance
+     */
+    @Contract("_ -> this")
+    public OutputButton setDisplayItemFunction(@NotNull BiFunction<UUID, ItemStack, ItemStack> displayItemFunction) {
+        this.displayItemFunction = displayItemFunction;
+        return this;
+    }
+}
