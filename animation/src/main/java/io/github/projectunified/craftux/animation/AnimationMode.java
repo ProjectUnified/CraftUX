930f8d428f0ed77b84da604b71936f3af1a0f39d
hsgamer <huynhqtienvtag@gmail.com>
2025-11-04 04:11:19 +0700

    fix javadoc

--- animation/src/main/java/io/github/projectunified/craftux/animation/AnimationMode.java
+++ animation/src/main/java/io/github/projectunified/craftux/animation/AnimationMode.java
@@ -9,11 +9,11 @@ public enum AnimationMode {
      */
     REPEAT,
     /**
-     * The {@link Animation#getCurrentFrame(long)}} will return {@link null} if the animation is completed ({@link Animation#isFirstRun(long)} returns {@link false})
+     * The {@link Animation#getCurrentFrame(long)}} will return "null" if the animation is completed ({@link Animation#isFirstRun(long)} returns false)
      */
     ONE_TIME,
     /**
-     * The {@link Animation#getCurrentFrame(long)}} will return the last element of the {@link Animation#getFrames()} if the animation is completed ({@link Animation#isFirstRun(long)} returns {@link false})
+     * The {@link Animation#getCurrentFrame(long)}} will return the last element of the {@link Animation#getFrames()} if the animation is completed ({@link Animation#isFirstRun(long)} returns false)
      */
     ONE_TIME_KEEP_LAST
 }
